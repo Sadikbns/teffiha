058011b19f92774349641a0a0542a818134ab8c1
Elseddik Bensadi <sadikbensadi7@gmail.com>
2026-07-30 20:10:54 +0100

    add production values

--- api/Program.cs
+++ api/Program.cs
@@ -33,7 +33,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5500",   // e.g. VS Code "Live Server" extension
-                "https://teffiha.com"
+                "https://teffiha.com",
+                "https://teffiha.sadikbensadi7.workers.dev/"
                 // //PROD: replace the two localhost origins above with your real
                 // deployed frontend domain, e.g. "https://balagh-algerie.example"
             )
